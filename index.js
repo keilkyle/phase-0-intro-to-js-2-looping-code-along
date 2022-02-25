b48c67386c66421f467ff4173781e1d678ec6df8
Christian Keil <christian@astranis.com>
2022-02-24 22:59:10 -0800

    commit tree 707d004d659ffff8345c2ffd0388eaafa4ac4709

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    const cards = [];
+    for (let name in names) {
+        cards.push(`Thank you, ${names[name]}, for the wonderful surprise gift!`);
+    }
+    return cards
+}
+
+function countDown (count) {
+    while (count + 1 > 0) {
+        console.log(count--);
+      }
+}
